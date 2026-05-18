4351ca4e0b21e97adf74dee32d4556008c790b65
aparnaavanapu <aparnaavanapu8@gmail.com>
2026-05-18 22:06:54 +0530

    upadted folders

--- Backend2/server.js
+++ Backend2/server.js
@@ -1,19 +1,11 @@
+// Hello Aparna API using Express.js
+
 const express = require("express");
 
 const app = express();
-const cors = require("cors");
 const PORT = 3000;
 
-app.use(cors());
-
-// API 1
-app.get("/hello", (req, res) => {
-  res.json({
-    message: "Hello World"
-  });
-});
-
-// API 2
+// Hello Aparna API
 app.get("/aparna", (req, res) => {
   res.json({
     message: "Hello Aparna"
